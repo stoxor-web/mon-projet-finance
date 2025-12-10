616555101149b90432dd4a365e9a32c50f815d85
Trendy World <lucas.scribe01@gmail.com>
2025-12-10 13:02:31 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App' // Il va chercher ton fichier App.tsx
 
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
